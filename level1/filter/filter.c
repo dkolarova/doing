342e995225b01357d3bb72c9f07f8d6d1f6eda0b
Diana Kolarova <136117521+dkolarova@users.noreply.github.com>
2026-01-21 18:24:12 +0100

    adding

--- level1/filter/filter.c
+++ level1/filter/filter.c
@@ -1,66 +1,35 @@
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <errno.h>
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 42
 #endif
 
-void *memmem(const void *, size_t, const void *, size_t);
-
-void ft_filter(char *s, char *pat)
+void ft_filter(char *s, char *f)
 {
-    size_t pl = strlen(pat);
-    char *p;
+	int i = 0, j, len = strlen(f);
 
-    while ((p = memmem(s, strlen(s), pat, pl)))
-    {
-        memmove(p + pl, p + pl, 0);
-        memset(p, '*', pl);
-        s = p + pl;
-    }
+	while (s[i])
+	{
+		j = 0;
+		while (f[j] && s[i + j] == f[j])
+			j++;
+		if (j == len)
+			while(j--) write(1, "*", 1), i++;
+		else
+			write(1, &s[i++], 1);
+	}
 }
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    char temp[BUFFER_SIZE];
-    char *res = NULL;
-    int total = 0;
-    ssize_t bytes;
+	char buf[99999];
+	int n, i = 0;
 
-    if (argc != 2 || !*argv[1])
-        return 1;
-
-    while ((bytes = read(0, temp, BUFFER_SIZE)) > 0)
-    {
-        char *buf = realloc(res, total + bytes + 1);
-        if (!buf)
-        {
-            perror("Error");
-            free(res);
-            return 1;
-        }
-        res = buf;
-        memmove(res + total, temp, bytes);
-        total += bytes;
-        res[total] = 0;
-    }
-
-    if (bytes < 0)
-    {
-        perror("Error");
-        free(res);
-        return 1;
-    }
-
-    if (res)
-    {
-        ft_filter(res, argv[1]);
-        write(1, res, total);
-    }
-
-    free(res);
-    return 0;
+	if (ac != 2 || !av[1][0])
+		return 1;
+	while ((n = read(0, buf + i, BUFFER_SIZE)) > 0)
+		i += n;
+	buf[i] = 0;
+	ft_filter(buf, av[1]);
 }
